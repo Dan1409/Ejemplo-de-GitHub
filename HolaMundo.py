6c00423201d98130c58492246275e4bfabe90046
Dan1409 <toresdaniela477@gmail.com>
2024-04-18 17:29:31 -0500

    feat: :zap: funcion recursiva
    funciona de suma y resta con recursividad

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,17 @@
-print("¡Hola, mundo!")
+def suma_recursiva(a, b):
+    if b == 0:
+        return a
+    else:
+        return suma_recursiva(a ^ b, (a & b) << 1)
+
+def resta_recursiva(a, b):
+    if b == 0:
+        return a
+    else:
+        return resta_recursiva(a ^ b, (~a & b) << 1)
+
+# Ejemplo de uso
+num1 = 10
+num2 = 5
+print("Suma de", num1, "+", num2, "=", suma_recursiva(num1, num2))
+print("Resta de", num1, "-", num2, "=", resta_recursiva(num1, num2))
